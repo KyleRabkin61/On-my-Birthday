fbecba5a2f1bc8b8e30e01cae2d241d08e08486a
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-01-24 11:55:50 -0500

    Java is cooking

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function change2007() {
+    document.getElementById('descrip').innerText = 'HIIIIIIIIIIIIIII';
+    document.body.style.backgroundColor = "#385170"
+    
+
+}
 function change1943() {
-    return document.getElementById('descrip').innerText = 'butthole'
+    document.getElementById('descrip').innerText = '1943 text'
+    document.body.style.backgroundColor = "#AA0000"
+    document.getElementById("navBar").style.backgroundColor = "#AA0000"
+    document.getElementById('description').style.backgroundColor = "#AA0000"
+    document.getElementById('image').style.backgroundColor = "#AA0000"
 }
